f1484657e749520f261f5c524a5793d2c38ccc14
IgnacioMilutin <i.milutin@alumno.um.edu.ar>
2024-06-23 02:05:51 -0300

    sin hora, dias funcionan, usuario no

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import pandas as pd
-from datetime import datetime
+from datetime import datetime,date,time
 import re
 
 data_df=pd.read_csv('export-2019-to-now-v4.csv')
-data_df['Inicio_de_Conexión_Dia'] = pd.to_datetime(data_df['Inicio_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce')
-data_df['Inicio_de_Conexión_Hora'] = pd.to_datetime(data_df['Inicio_de_Conexión_Hora'], format='%H-%M-%S', errors='coerce')
-data_df['FIN_de_Conexión_Dia'] = pd.to_datetime(data_df['FIN_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce')
-data_df['FIN_de_Conexión_Hora'] = pd.to_datetime(data_df['FIN_de_Conexión_Hora'], format='%H-%M-%S', errors='coerce')
+data_df['Inicio_de_Conexión_Dia'] = pd.to_datetime(data_df['Inicio_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce').dt.date
 
 #print(data_df.dtypes)
 
@@ -24,42 +21,33 @@ def validacion_fecha(fecha):
             fecha=datetime.strptime(fecha,'%Y-%m-%d').date()
             return fecha
         except ValueError:
-            return print('fecha no valida')
-    else: return print('fecha no valida')
+            print('fecha no valida')
+            return None
+    else: 
+        print('fecha no valida')
+        return None
 
-def validacion_hora(hora):
-    hora_re=r"^\d{2}:\d{2}:\d{2}$"
-    if re.match(hora_re,hora):
-        try:
-            hora=datetime.strptime(hora,'%H:%M:%S').time()
-            return hora
-        except ValueError:
-            return print('hora no valida')
-    else: return print('hora no valida')
 
-def validacion_usuario(usuario):
-    pass
-
-def filtro(usuario,fecha_inicio,hora_inicio,fecha_fin,hora_fin):
-    filtro=((data_df['Inicio_de_Conexión_Dia']>=fecha_inicio) &
-            (data_df['Inicio_de_Conexión_Hora']>=hora_inicio) &
-            (data_df['Inicio_de_Conexión_Dia']<=fecha_fin) &
-            (data_df['Inicio_de_Conexión_Hora']<=hora_fin) &
-            (data_df['Usuario']==usuario))
-    archivo_filtrado=data_df[filtro]
+def validacion_usuario(df,usuario):
+    if df['Usuario'].isin([usuario]).any():
+        return usuario
+    else: 
+        print('usuario inexistente')
+        return None
+    
+def filtro(df,fecha_inicio,fecha_fin):
+    archivo_filtrado=df[(df['Inicio_de_Conexión_Dia']>=fecha_inicio) &
+            (df['Inicio_de_Conexión_Dia']<=fecha_fin) 
+            ]
     print(archivo_filtrado)
     return archivo_filtrado
 
-usuario=input('ingrese el usuario al que desea filtrar: ')
+#usuario=input('ingrese el usuario al que desea filtrar: ')
 fecha_inicio=input('ingrese la fecha desde donde se quiere analizar: ')
-hora_inicio=input('ingrese la hora desde donde se quiere analizar: ')
 fecha_fin=input('ingrese la fecha hasta la cual se quiere analizar: ')
-hora_fin=input('ingrese la hora hasta la cual se quiere analizar: ')
 
 fecha_inicio=validacion_fecha(fecha_inicio)
-hora_inicio=validacion_hora(hora_inicio)
 fecha_fin=validacion_fecha(fecha_fin)
-hora_fin=validacion_hora(hora_fin)
-usuario=validacion_usuario(usuario)
+#usuario=validacion_usuario(data_df,usuario)
 
-filtro(usuario,fecha_inicio,hora_inicio,fecha_fin,hora_fin)
+filtro(data_df,fecha_inicio,fecha_fin)
--- test.py
+++ test.py
@@ -9,25 +9,25 @@ from main import filtro, validacion_fecha, validacion_hora
    # def test_1disp(self):
     #    self.assertEqual()
 
-data_df=pd.read_csv('test_data.csv')
+test_df=pd.read_csv('test_data.csv')
 
-data_df['Inicio_de_Conexión_Dia'] = pd.to_datetime(data_df['Inicio_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce').dt.date
-data_df['Inicio_de_Conexión_Hora'] = pd.to_datetime(data_df['Inicio_de_Conexión_Hora'], format='%H-%M-%S', errors='coerce').dt.time
-data_df['FIN_de_Conexión_Dia'] = pd.to_datetime(data_df['FIN_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce').dt.date
-data_df['FIN_de_Conexión_Hora'] = pd.to_datetime(data_df['FIN_de_Conexión_Hora'], format='%H-%M-%S', errors='coerce').dt.time
+test_df['Inicio_de_Conexión_Dia'] = pd.to_datetime(test_df['Inicio_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce').dt.date
+test_df['Inicio_de_Conexión_Hora'] = pd.to_datetime(test_df['Inicio_de_Conexión_Hora'], format='%H-%M-%S', errors='coerce').dt.time
+test_df['FIN_de_Conexión_Dia'] = pd.to_datetime(test_df['FIN_de_Conexión_Dia'], format='%Y-%m-%d', errors='coerce').dt.date
+test_df['FIN_de_Conexión_Hora'] = pd.to_datetime(test_df['FIN_de_Conexión_Hora'], format='%H-%M-%S', errors='coerce').dt.time
 
-print(data_df.dtypes)
+print(test_df.dtypes)
 
-usuario=input('ingrese el usuario al que desea filtrar: ')
-fecha_inicio=input('ingrese la fecha desde donde se quiere analizar: ')
-hora_inicio=input('ingrese la hora desde donde se quiere analizar: ')
-fecha_fin=input('ingrese la fecha hasta la cual se quiere analizar: ')
-hora_fin=input('ingrese la hora hasta la cual se quiere analizar: ')
+usuario_test=input('ingrese el usuario al que desea filtrar: ')
+fecha_inicio_test=input('ingrese la fecha desde donde se quiere analizar: ')
+hora_inicio_test=input('ingrese la hora desde donde se quiere analizar: ')
+fecha_fin_test=input('ingrese la fecha hasta la cual se quiere analizar: ')
+hora_fin_test=input('ingrese la hora hasta la cual se quiere analizar: ')
 
-fecha_inicio=validacion_fecha(fecha_inicio)
-hora_inicio=validacion_hora(hora_inicio)
-fecha_fin=validacion_fecha(fecha_fin)
-hora_fin=validacion_hora(hora_fin)
+fecha_inicio=validacion_fecha(fecha_inicio_test)
+hora_inicio=validacion_hora(hora_inicio_test)
+fecha_fin=validacion_fecha(fecha_fin_test)
+hora_fin=validacion_hora(hora_fin_test)
 #usuario=validacion_usuario(usuario)
 
-filtro(usuario,fecha_inicio,hora_inicio,fecha_fin,hora_fin)
+filtro(test_df,usuario_test,fecha_inicio_test,hora_inicio_test,fecha_fin_test,hora_fin_test)
